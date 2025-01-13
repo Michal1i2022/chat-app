7935963c85b0b23761a43bed92d5814ce8e4efcc
Michal1i2022 <1i2022michal29@gmail.com>
2025-01-13 16:22:45 +0100

    1.3

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/chat-app', {
+        await mongoose.connect('mongodb+srv://michalmakowka420:nGPiV0J8VpxvuSwg@cluster0.ytq0c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
